13ae680ac495cd00bef5fe63dcd8553f44330d0d
Yaroslav Berkuta <yaroslavberkuta@gmail.com>
2021-03-03 16:55:26 +0200

    srvise

--- publick/js/main.js
+++ publick/js/main.js
@@ -1,1 +1,10 @@
 var hellopreloader = document.getElementById("hellopreloader_preload");function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
+
+$(function() {
+    // плавное перемещение страницы к нужному блоку
+	$("nav li a").click(function () {
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 1500);
+	});
+});

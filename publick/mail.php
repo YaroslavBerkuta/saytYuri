f2df967d2621898177926b7e94682d3ba3a6e13b
Yaroslav Berkuta <yaroslavberkuta@gmail.com>
2021-03-06 20:45:45 +0200

    форма

---
+++ publick/mail.php
@@ -0,0 +1,15 @@
+<?php  
+	$header = "Content-type: text/html; charset=utf-8";
+	$subject = "the subject";
+ 
+	$msg 	= "Form Contents: \n\n";
+	foreach ($_POST as $key=>$val)
+	{
+		$msg .= "$key :  $val \n";
+	}
+ 
+	$from = "yaroslavberkuta@gmail.com"; // Кому
+	mail($from,$subject,$msg,$header);
+	header("Location: $_SERVER[HTTP_REFERER]");
+ 
+?>
